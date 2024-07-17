87b325fc6d844bcbca54b05c7ee89079d9843af4
praveenkumar563777 <praveenkumar563777@gmail.com>
2024-07-18 00:00:18 +0530

    -index file done

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+const database_Url = process.env.DATABASE_URL;
+const dbName = 'todo';
+
+mongoose.connect(database_Url, {useNewUrlParser: true, useUnifiedTopology: true});
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+db.once('open', ()=>{
+    console.log('connected to MongoDB', dbName );
+});
 
 app.get('/', function (req, res) {
-  res.send('Hello to the world of Corporate Cubicles!!');
-})
+  res.send('Hello to the world of Corporate Cubicles!!!');
+});
 
-const portNo = 3000;
+const portNo = process.env.PORT_NO;
 app.listen(portNo, ()=>{
-    console.log(` Application running on Port Number ${portNo}`);
+    console.log(` Application running on Port Numbers ${portNo}`);
 });
